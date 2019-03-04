2514f6205758c4fea8436ef7fba575740ead51b1
Acquaviva Marco <acquavivamarco@hotmail.it>
2019-03-04 12:41:01 +0100

    Add approximate_size keywords and return documantation

--- humansize.py
+++ humansize.py
@@ -3,6 +3,10 @@ SUFFIXES = ['KB', 'MB','GB','TB','PB','EB','ZB','YB']
 
 def approximate_size(size):
     """Convert a file size to human-readable form.
+    Keyword arguments: 
+    size -- file size in bytes
+
+    Returns: string
     """
     multiple = 1024 
     for suffix in SUFFIXES:
